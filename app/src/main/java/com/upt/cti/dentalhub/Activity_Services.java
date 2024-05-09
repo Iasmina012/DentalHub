d575f54d7e9162489054ffdc7bf3ec5b96069d00
iasmina012 <iasmina.putina012@yahoo.com>
2024-05-09 15:51:41 +0300

    Fixed Services Activity

--- app/src/main/java/com/upt/cti/dentalhub/Activity_Services.java
+++ app/src/main/java/com/upt/cti/dentalhub/Activity_Services.java
@@ -2,9 +2,7 @@ package com.upt.cti.dentalhub;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
-import android.widget.LinearLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
