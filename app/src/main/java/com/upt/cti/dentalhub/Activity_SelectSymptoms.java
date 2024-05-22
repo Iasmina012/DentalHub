b7c365e495509dae6ff495595294e1116311c1f8
iasmina012 <iasmina.putina012@yahoo.com>
2024-05-22 21:32:08 +0300

    Updated Gradle

--- app/src/main/java/com/upt/cti/dentalhub/Activity_SelectSymptoms.java
+++ app/src/main/java/com/upt/cti/dentalhub/Activity_SelectSymptoms.java
@@ -6,8 +6,10 @@ import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.Toast;
+
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
